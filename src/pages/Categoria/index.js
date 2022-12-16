3076ebdd042884d35151de83fa75d9b520672c75
unknown <cesardanielmg@hotmail.com>
2022-12-16 12:46:18 -0300

    primeiro commit

---
+++ src/pages/Categoria/index.js
@@ -0,0 +1,35 @@
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import Header from "../../components/Header";
+import Item from "../../components/Item";
+import styles from './Categoria.module.scss'
+
+export default function Categoria() {
+    const {nomeCategoria} = useParams();
+    const {categoria, itens} =  useSelector((state) =>({
+      
+            categoria: state.categorias.find((categoria) => categoria.id === nomeCategoria),
+            itens: state.itens.filter((item) => item.categoria === nomeCategoria)
+     
+    } ))
+    console.log(nomeCategoria)
+    console.log(itens)
+  return (
+    <div>
+        <Header 
+        titulo={categoria.nome}
+        descricao={categoria.descricao}
+        imagem={categoria.header}
+        />
+        <div className={styles.itens}>
+            {itens?.map((item) =>(
+                
+                <Item key={item.id} {...item}/>
+                
+            ))}
+        </div>
+    </div>
+
+   
+  )
+}
---
+++ src/routes.js
@@ -0,0 +1,20 @@
+
+
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import PaginaPadrao from './components/PaginaPadrao'
+import Categoria from './pages/Categoria'
+import Home from './pages/Home'
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<PaginaPadrao/>}>
+             <Route index element={<Home/>} />
+             <Route path='/categoria/:nomeCategoria' element={<Categoria/>} />
+            </Route>             
+        </Routes>
+    </BrowserRouter>
+  )
+}
